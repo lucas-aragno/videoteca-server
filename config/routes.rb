e4e580cf82001ae315cc2891b9795c4cf63fa33b
Lucas Aragno <laragno@Lucass-MacBook-Air.local>
2015-10-03 20:29:31 -0300

    fixing up activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
-  devise_for :users, ActiveAdmin::Devise.config
   devise_for :admin_users, ActiveAdmin::Devise.config
-
+  ActiveAdmin.routes(self)
   mount Videoteca::API => '/v1'
-  
-  root :to => redirect('/admin')
 end
