ad620535675a638804116f2abb90beb30533f56c
Lucas Aragno <laragno@Lucass-MacBook-Air.local>
2015-10-03 16:50:41 -0300

    workaround on login issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :check_for_admin
   protect_from_forgery with: :exception
+
+  def check_for_admin
+  	redirect_to '/admin/login' if (!request.fullpath.eql?('/admin/login') and current_admin_user.nil?)
+  end
 end
